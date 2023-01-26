2153f107dd4390e3d456f1d905407760bfdc06cf
Juyotal <cesarkamga3@gmail.com>
2023-01-26 22:50:14 +0200

    romans aditional soln

--- romans.py
+++ romans.py
@@ -8,3 +8,12 @@ def create_roman(value):
             roman += letters
             value -= val
     return roman
+
+
+units = " I II III IV V VI VII VIII IX".split(" ")
+tens = " X XX XXX XL L LX LXX LXXX XC".split(" ")
+hundreds = " C CC CCC CD D DC DCC DCCC CM".split(" ")
+thousands = " M MM MMM".split(" ")
+
+def solution(n):
+    return thousands[n//1000] + hundreds[n%1000//100] + tens[n%100//10] + units[n%10]
